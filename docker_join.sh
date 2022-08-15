3828700596ba1978f505240d4077e2062e0dab9a
sunfu-chou <sunfu.chou@gmail.com>
2022-08-15 17:12:44 +0800

    Feat: Add docker_join.sh

---
+++ docker_join.sh
@@ -0,0 +1,11 @@
+IMG=sunfuchou/vr_slam_uwb:jackal-nuc
+
+xhost +
+containerid=$(docker ps -aqf "ancestor=${IMG}") && echo $containerid
+docker exec -it \
+    --privileged \
+    -e DISPLAY=${DISPLAY} \
+    -e LINES="$(tput lines)" \
+    ${containerid} \
+    bash
+xhost -
