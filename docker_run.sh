3c0494910d198221339ac491268adcf7b32ae1ea
sunfu-chou <sunfu.chou@gmail.com>
2022-08-15 13:52:59 +0800

    Feat: Add submodule jackal and jackal_robot

---
+++ docker_run.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+ARGS=("$@")
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]; then
+    xauth_list=$(xauth nlist $DISPLAY)
+    xauth_list=$(sed -e 's/^..../ffff/' <<<"$xauth_list")
+    if [ ! -z "$xauth_list" ]; then
+        echo "$xauth_list" | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+docker run \
+    -it \
+    --rm \
+    -e DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -e XAUTHORITY=$XAUTH \
+    -v "$XAUTH:$XAUTH" \
+    -v "/home/$USER/vr-slam-uwb:/home/arg/vr-slam-uwb" \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+    -v "/etc/localtime:/etc/localtime:ro" \
+    -v "/dev:/dev" \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
+    -v "/home/$USER/.bashrc:/home/argsubt/.bashrc" \
+    -v "/media/yellow/arg2TB-Ray:/home/argsubt/SSD" \
+    --user "root:root" \
+    --workdir "/home/arg/vr-slam-uwb" \
+    --name vr-slam-uwb \
+    --network host \
+    --privileged \
+    --security-opt seccomp=unconfined \
+    sunfu/vr_slam_uwb:jackal-nuc \
+    bash
