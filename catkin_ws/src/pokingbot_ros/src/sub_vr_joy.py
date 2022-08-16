90e10f227815dc2cb898ca4a114a4ded1fe9dc9e
sunfu-chou <sunfu.chou@gmail.com>
2022-08-16 23:53:44 +0800

    Refator: Add jackal ns

--- catkin_ws/src/pokingbot_ros/src/sub_vr_joy.py
+++ catkin_ws/src/pokingbot_ros/src/sub_vr_joy.py
@@ -9,14 +9,14 @@ class subvrandpub(object):
  
     def __init__(self):
         # robot1
-        self.sub_xy1 = rospy.Subscriber("vr/joystick_xy", Float32MultiArray, self.callback_xy1)
-        self.pub_twist1 = rospy.Publisher("joy_teleop/cmd_vel",Twist, queue_size=10)
+        self.sub_xy1 = rospy.Subscriber("/vr/joystick_xy", Float32MultiArray, self.callback_xy1)
+        self.pub_twist1 = rospy.Publisher("/robot1/cmd_vel",Twist, queue_size=10)
         # # robot2
-        self.sub_xy2 = rospy.Subscriber("vr2/joystick_xy", Float32MultiArray, self.callback_xy2)
-        self.pub_twist2 = rospy.Publisher("/robot2/husky_velocity_controller/cmd_vel",Twist, queue_size=10)
+        self.sub_xy2 = rospy.Subscriber("/vr2/joystick_xy", Float32MultiArray, self.callback_xy2)
+        self.pub_twist2 = rospy.Publisher("/robot2/cmd_vel",Twist, queue_size=10)
         # # robot3
-        self.sub_xy3 = rospy.Subscriber("vr3/joystick_xy", Float32MultiArray, self.callback_xy3)
-        self.pub_twist3 = rospy.Publisher("/robot3/husky_velocity_controller/cmd_vel",Twist, queue_size=10)
+        self.sub_xy3 = rospy.Subscriber("/vr3/joystick_xy", Float32MultiArray, self.callback_xy3)
+        self.pub_twist3 = rospy.Publisher("/robot3/cmd_vel",Twist, queue_size=10)
  
     def callback_xy1(self, data):
         cmd_vel = Twist()
