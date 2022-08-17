bae232089f67fd59eb4f5b3d691d134aae79e827
sunfu-chou <sunfu.chou@gmail.com>
2022-08-17 17:16:07 +0800

    Feat: Add tf_prefix

--- catkin_ws/src/mapping_and_odometry/mapping/src/odom_tf.py
+++ catkin_ws/src/mapping_and_odometry/mapping/src/odom_tf.py
@@ -12,37 +12,23 @@ import rospkg
 from nav_msgs.msg import Odometry
 
 
-class Wheel_tf():
+class Wheel_tf:
     def __init__(self):
         self.br = tf.TransformBroadcaster()
         self.origin_matrix = None
-        odom_sub = rospy.Subscriber(
-            "wheel_odom", Odometry, self.cb_odom, queue_size=1)
+        odom_sub = rospy.Subscriber("wheel_odom", Odometry, self.cb_odom, queue_size=1)
 
     def cb_odom(self, msg):
         if self.origin_matrix is None:
             self.origin_matrix = tf.transformations.quaternion_matrix(
-                [
-                    msg.pose.pose.orientation.x,
-                    msg.pose.pose.orientation.y,
-                    msg.pose.pose.orientation.z,
-                    msg.pose.pose.orientation.w
-                ]
+                [msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]
             )
             self.origin_matrix[0, 3] = msg.pose.pose.position.x
             self.origin_matrix[1, 3] = msg.pose.pose.position.y
             self.origin_matrix[2, 3] = msg.pose.pose.position.z
-            self.origin_matrix = tf.transformations.inverse_matrix(
-                self.origin_matrix)
+            self.origin_matrix = tf.transformations.inverse_matrix(self.origin_matrix)
         else:
-            new_matrix = tf.transformations.quaternion_matrix(
-                [
-                    msg.pose.pose.orientation.x,
-                    msg.pose.pose.orientation.y,
-                    msg.pose.pose.orientation.z,
-                    msg.pose.pose.orientation.w
-                ]
-            )
+            new_matrix = tf.transformations.quaternion_matrix([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
             new_matrix[0, 3] = msg.pose.pose.position.x
             new_matrix[1, 3] = msg.pose.pose.position.y
             new_matrix[2, 3] = msg.pose.pose.position.z
@@ -52,17 +38,20 @@ class Wheel_tf():
             quat = tf.transformations.quaternion_from_matrix(new_matrix)
 
             self.br.sendTransform(
+                (new_matrix[0, 3], new_matrix[1, 3], new_matrix[2, 3]),
                 (
-                    new_matrix[0, 3], new_matrix[1, 3], new_matrix[2, 3]
+                    quat[0],
+                    quat[1],
+                    quat[2],
+                    quat[3],
                 ),
-                (
-                    quat[0], quat[1], quat[2], quat[3],
-                ),
-                msg.header.stamp, "base_link", "odom"
+                msg.header.stamp,
+                "base_link",
+                "odom",
             )
 
 
-if __name__ == '__main__':
-    rospy.init_node('odom_tf')
+if __name__ == "__main__":
+    rospy.init_node("odom_tf")
     wheel_tf = Wheel_tf()
     rospy.spin()
