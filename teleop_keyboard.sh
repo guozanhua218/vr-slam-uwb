b956820d4254c28ec34bcc43080657b2ab93f8a1
sunfu-chou <sunfu.chou@gmail.com>
2022-08-24 17:41:00 +0800

    Feat: Add teleop_keyboard shell script

---
+++ teleop_keyboard.sh
@@ -0,0 +1,9 @@
+#!  /bin/bash
+
+rosrun teleop_twist_keyboard teleop_twist_keyboard.py \
+  cmd_vel:=jackal/teleop_keyboard/cmd_vel \
+  _speed:=0.2 \
+  _turn:=0.4 \
+  _repeat_rate:=10 \
+  _key_timeout:=0.2 \
+
