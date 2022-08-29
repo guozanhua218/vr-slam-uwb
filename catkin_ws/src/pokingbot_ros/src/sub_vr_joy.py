10b30c58562e56063304c985740cdbc915f68657
sunfu-chou <sunfu.chou@gmail.com>
2022-08-29 13:49:08 +0800

    Refactor: Refactor sub_vr_joy.py

--- catkin_ws/src/pokingbot_ros/src/sub_vr_joy.py
+++ catkin_ws/src/pokingbot_ros/src/sub_vr_joy.py
@@ -1,46 +1,30 @@
 #! /usr/bin/env python
 
 import rospy
-import math
 from std_msgs.msg import Float32MultiArray
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Twist
 
-class subvrandpub(object):
- 
+
+class VRJSToCmdVel(object):
     def __init__(self):
-        # robot1
-        self.sub_xy1 = rospy.Subscriber("/vr/joystick_xy", Float32MultiArray, self.callback_xy1)
-        self.pub_twist1 = rospy.Publisher("/robot1/cmd_vel",Twist, queue_size=10)
-        # # robot2
-        self.sub_xy2 = rospy.Subscriber("/vr2/joystick_xy", Float32MultiArray, self.callback_xy2)
-        self.pub_twist2 = rospy.Publisher("/robot2/cmd_vel",Twist, queue_size=10)
-        # # robot3
-        self.sub_xy3 = rospy.Subscriber("/vr3/joystick_xy", Float32MultiArray, self.callback_xy3)
-        self.pub_twist3 = rospy.Publisher("/robot3/cmd_vel",Twist, queue_size=10)
- 
-    def callback_xy1(self, data):
+        self.sub_js = rospy.Subscriber("vr/joystick_xy", Float32MultiArray, self.callback_js)
+        self.pub_twist = (
+            rospy.Publisher("robot1/cmd_vel", Twist, queue_size=10),
+            rospy.Publisher("robot2/cmd_vel", Twist, queue_size=10),
+            rospy.Publisher("robot3/cmd_vel", Twist, queue_size=10),
+            rospy.Publisher("robot4/cmd_vel", Twist, queue_size=10),
+        )
+
+    def callback_js(self, data):
         cmd_vel = Twist()
-        print(data.data[0], data.data[1])
         cmd_vel.angular.z = -data.data[0]
-        cmd_vel.linear.x= data.data[1]
-        self.pub_twist1.publish(cmd_vel)
-
-    def callback_xy2(self, data):
-        cmd_vel = Twist()
-        print(data.data[0], data.data[1])
-        cmd_vel.angular.z = -data.data[0]
-        cmd_vel.linear.x= data.data[1]
-        self.pub_twist2.publish(cmd_vel)
-
-    def callback_xy3(self, data):
-        cmd_vel = Twist()
-        print(data.data[0], data.data[1])
-        cmd_vel.angular.z = -data.data[0]
-        cmd_vel.linear.x= data.data[1]
-        self.pub_twist3.publish(cmd_vel)
+        cmd_vel.linear.x = data.data[1]
+        for i in range(len(data.data) - 4):
+            if data.data[i + 2]:
+                self.pub_twist[i].publish(cmd_vel)
 
 
-if __name__ == '__main__':
-    rospy.init_node('listener', anonymous=True)
-    SUB = subvrandpub()
-    rospy.spin() #run forever
+if __name__ == "__main__":
+    rospy.init_node("sub_vr_joy", anonymous=True)
+    vr_js_to_cmd_vel = VRJSToCmdVel()
+    rospy.spin()
