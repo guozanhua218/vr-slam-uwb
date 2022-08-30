1ee3f560c92fac117293bc39731b3d139c4852ad
sunfu-chou <sunfu.chou@gmail.com>
2022-08-30 14:12:13 +0800

    Feat: Add veh support for env.sh and teleop.sh

--- environment.sh
+++ environment.sh
@@ -1,31 +1,75 @@
 #! /bin/bash
 
-URI=127.0.0.1
-IP=127.0.0.1
+URI=""
+IP=""
+
+MASTER=""
+HOST=""
+CONFIG=""
 
 if [ "$1" ]; then
+  MASTER="$1"
+fi
 
-  echo "$1"
-  URI=127.0.0.1
-  IP=127.0.0.1
+if [ "$2" ]; then
+  HOST="$2"
+fi
 
-  if [ "$1" == "wan" ]; then
-    URI=111.70.9.27
-    IP=111.70.9.27
+if [ "$2" ]; then
+  CONFIG="$3"
+fi
 
-  elif [ "$1" == "lan" ]; then
-    URI=192.168.0.100
-    IP=192.168.0.100
 
-  else
+if [ "$CONFIG" == "wan" ]; then
+  # MASTER
+  if [ "$MASTER" == "jackal" ]; then
+    URI="111.70.9.27"
 
-    if [ "$2" ]; then
-      URI=$1
-      IP=$2
-    fi
-
+  elif [ "$MASTER" == "husky" ]; then
+    URI="111.70.9.53"
   fi
 
+  # HOST
+  if [ "$HOST" == "jackal" ]; then
+    IP="111.70.9.27"
+
+  elif [ "$HOST" == "husky" ]; then
+    IP="111.70.9.53"
+
+  elif [ "$HOST" == "pc" ]; then
+    IP=$(/sbin/ip -o -4 addr list wlo1 | awk '{print $4}' | cut -d/ -f1)
+  fi
+fi
+
+if [ "$CONFIG" == "lan" ]; then
+  # MASTER
+  if [ "$MASTER" == "jackal" ]; then
+    URI="192.168.0.100"
+
+  elif [ "$MASTER" == "husky" ]; then
+    URI="192.168.0.104"
+  fi
+
+  # HOST
+  if [ "$HOST" == "jackal" ]; then
+    IP="192.168.0.100"
+
+  elif [ "$HOST" == "husky" ]; then
+    IP="192.168.0.104"
+
+  elif [ "$HOST" == "pc" ]; then
+    IP=$(/sbin/ip -o -4 addr list wlo1 | awk '{print $4}' | cut -d/ -f1)
+  fi
+fi
+
+if [ -z "$URI" ]; then
+  echo "ROS MASTER NOT SET. DEFAULTS TO 127.0.0.1"
+  URI=127.0.0.1
+fi
+
+if [ -z "$IP" ]; then
+  echo "ROS IP NOT SET. DEFAULTS TO 127.0.0.1"
+  IP=127.0.0.1
 fi
 
 export ROS_MASTER_URI="http://${URI}:11311"
@@ -34,5 +78,15 @@ export ROS_IP=$IP
 echo "ROS_MASTER_URI=${ROS_MASTER_URI}"
 echo "ROS_IP=${ROS_IP}"
 
-source /opt/ros/melodic/setup.bash
-source catkin_ws/devel/setup.bash
+if [ -f /opt/ros/melodic/setup.bash ]; then
+  echo "source: /opt/ros/melodic/setup.bash"
+  source /opt/ros/melodic/setup.bash
+elif [ -f /opt/ros/noetic/setup.bash ]; then
+  echo "source: /opt/ros/noetic/setup.bash"
+  source /opt/ros/noetic/setup.bash
+fi
+
+if [ -f ./catkin_ws/devel/setup.bash ]; then
+  echo "./catkin_ws/devel/setup.bash"
+  source ./catkin_ws/devel/setup.bash
+fi
--- teleop_keyboard.sh
+++ teleop_keyboard.sh
@@ -1,7 +1,11 @@
 #!  /bin/bash
 
+if [ "$1" ]; then
+  veh="$1"
+fi
+
 rosrun teleop_twist_keyboard teleop_twist_keyboard.py \
-  cmd_vel:=jackal/teleop_keyboard/cmd_vel \
+  cmd_vel:="${veh}"/teleop_keyboard/cmd_vel \
   _speed:=0.2 \
   _turn:=0.4 \
   _repeat_rate:=10 \
